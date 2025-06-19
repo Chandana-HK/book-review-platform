2dc2824e15dc3e973cf2e321ad785e569d5d7e22
Chandana-HK <chandanahk13@gmail.com>
2025-06-19 17:42:51 +0530

    Add files via upload

---
+++ Book.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  author: String,
+  description: String,
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5
+  }
+});
+
+module.exports = mongoose.model('Book', bookSchema);
